fdd134fe245cdc830aca49c9326bd49d2453b011
hoatenhu <lenhuhoa1003@gmail.com>
2022-06-05 22:10:54 +0700

    fix eslint

--- components/navbar.js
+++ components/navbar.js
@@ -16,7 +16,7 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
+import { IoLogoLinkedin } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
