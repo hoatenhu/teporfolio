d13c6882c3d88306f5645b0a8d8a34d28b383a8a
hoatenhu <lenhuhoa1003@gmail.com>
2022-06-08 22:42:10 +0700

    delete images p1

--- pages/posts.js
+++ pages/posts.js
@@ -3,14 +3,12 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbPortfolio from '../public/images/contents/auto.jpeg'
-import thumbHowToUseInkdrop from '../public/images/contents/youtube-how-to-use-inkdrop.jpg'
-import thumbFishWorkflow from '../public/images/contents/config.jpeg'
-import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
-import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
-import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
-import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
-import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
+import thumbConfigWifi from '../public/images/contents/config.jpeg'
+import thumbFixInclude from '../public/images/contents/fixinclude.png'
+import thumAuto from '../public/images/contents/auto.jpeg'
+import thumbDesign from '../public/images/contents/design.jpg'
+import thumbControlLed from '../public/images/contents/led.jpg'
+import thumbSht10 from '../public/images/contents/sht10.jpg'
 
 const Posts = () => (
   <Layout title="Posts">
@@ -23,22 +21,22 @@ const Posts = () => (
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             title="ESP32-IDF: Configure WiFi by softAP mode and run Webserver control devices with smartphone"
-            thumbnail={thumbFishWorkflow}
+            thumbnail={thumbConfigWifi}
             href="https://www.youtube.com/watch?v=7quaN0E-GcY&ab_channel=Te"
           />
           <GridItem
             title="ESP32-IDF: Fix #include error in VS Code vs ESP-IDF file"
-            thumbnail={thumbHowToUseInkdrop}
+            thumbnail={thumbFixInclude}
             href="https://www.youtube.com/watch?v=uOq4U5B-hWk&t=31s&ab_channel=Te"
           />
           <GridItem
             title="ESP32-IDF: test Autoconnect webserver Captive Portal example"
-            thumbnail={thumbPortfolio}
+            thumbnail={thumAuto}
             href="https://www.youtube.com/watch?v=Tunxs0FKR0o&t=6s&ab_channel=Te"
           />
           <GridItem
             title="Design UI IoT for ESP32 project. This can help farmers take care of automatic farm"
-            thumbnail={thumbMyDeskSetup}
+            thumbnail={thumbDesign}
             href="https://www.youtube.com/watch?v=BIEz4EbM-DA&t=5s&ab_channel=Te"
           />
         </SimpleGrid>
@@ -47,13 +45,13 @@ const Posts = () => (
       <Section delay={0.3}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
-            title="ESP32-IDF: push data to Thingspeak provsion by smartphone"
-            thumbnail={thumb500PaidUsers}
-            href="https://www.youtube.com/watch?v=ndI3iU1LvTc&ab_channel=Te"
+            title="ESP32-IDF: Control LED via MQTT and update to Webserver"
+            thumbnail={thumbControlLed}
+            href="https://www.youtube.com/watch?v=3rNjURxppTE&t=27s"
           />
           <GridItem
             title="ESP32-IDF: Display SHT10 data via HTTP with station mode, turn lights on and pull LED brightness.."
-            thumbnail={thumbFinancialGoal}
+            thumbnail={thumbSht10}
             href="https://blog.inkdrop.app/i-stopped-setting-a-financial-goal-for-my-saas-a92c3db65506"
           />
         </SimpleGrid>
